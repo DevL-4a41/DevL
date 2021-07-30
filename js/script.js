850b0491e734f64bbc38ffbd3197d31d9039e505
DevL-4a41 <4a41devl@gmail.com>
2021-07-30 13:43:47 +0600

    prebuilt-1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*const setContainerWidth = () =>
+{
+  let elem = document.querySelector('body');
+  let wd = window.getComputedStyle(elem, null).getPropertyValue("width");
+  
+  let container = document.querySelector("#container");
+  let str = wd;
+  var matches = str.match(/(\d+)/);
+  let new_value;
+  if (matches)
+    new_value = matches[0];
+  container.style.width = new_value+"px";
+}*/
+
