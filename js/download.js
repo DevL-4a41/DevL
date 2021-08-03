3e4e10fd3a4081c7428cb9e8045ed6a1235bc6cc
AbdullahBinJahed <abdullahbinjahed35@gmail.com>
2021-08-03 14:39:20 +0600

    download page now at least does something

--- js/download.js
+++ js/download.js
@@ -1,1 +1,14 @@
-console.log(LN_Data);
+var dname = localStorage.getItem('dname');
+var dvol = localStorage.getItem('dvol');
+var newElem = "";
+var dSec = document.querySelector(".d-section");
+for (let i = 1; i <= dvol; i++)
+{
+  newElem += "<div class='d-cont'>";
+  newElem += "<img class='d-cover' src='./assets/drawable/spinner.gif' />";
+  newElem += "<div class='d-btn'>";
+  newElem += "<a href='#'>Download "+dname+" Vol " + i + "</a>";
+  newElem += "</div> </div>";
+}
+
+dSec.innerHTML = newElem;
